8c7396ca0c9757bb982e88ee91ac2dbb7f839d5a
Diana <Diana@DESKTOP-RLN4LA8>
2018-03-27 19:33:16 +0400

    user identity

---
+++ DistanceLearningFinal/Models/Faculty.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DistanceLearningFinal.Models
+{
+    public class Faculty
+    {
+        public int FacultyID { set; get; }
+        public string Name { set; get; }
+    }
+}
--- DistanceLearningFinal/Models/IdentityModels.cs
+++ DistanceLearningFinal/Models/IdentityModels.cs
@@ -20,6 +20,7 @@ namespace DistanceLearningFinal.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<Faculty> Faculties { get; set; }
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
